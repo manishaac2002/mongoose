09201d8fc66b5108e3c656072477b64858c38cde
manishaac2002 <manishaac2002@gmail.com>
2024-04-02 12:31:25 +0530

    feat : Add a schema to the database.

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ async function main() {
   const connection = await mongoose.connect("mongodb://dbOwner:password@13.201.218.99:27017/dev-sachin")
   if (connection) {
     console.log("Database is connected successfully..")
+    //read the query  
+    const user = await schema.personModel.findOne({name:"Nisha"})
+    console.log(user);
   } else {
     console.log("Database is not connected")
   }
 }
 
+//save the user
 const user = new schema.personModel({name:"Nisha",age:23})
-user.save().then(()=>{console.log("User saved");})
+user.save().then(()=>{console.log("User saved")})
 
 main()
